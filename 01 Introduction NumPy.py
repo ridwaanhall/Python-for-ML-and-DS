8b25ca831867342eb76d059aa2946eda722e42b8
ridwaanhall <erbyl14@gmail.com>
2023-11-03 16:13:05 +0700

    Adds introduction numpy

---
+++ 01 Introduction NumPy.py
@@ -0,0 +1,15 @@
+# %%
+import numpy as np
+
+# %%
+# create 2 new lists height and weight
+person_weight = [60, 72, 57, 90, 95, 72]
+person_height = [1.6, 1.8, 1.5, 1.7, 1.9, 1.8]
+
+# create 2 numpy arrays from height and weight
+person_height_np = np.array(person_height)
+person_weight_np = np.array(person_weight)
+
+# %%
+person_height_np
+# %%
